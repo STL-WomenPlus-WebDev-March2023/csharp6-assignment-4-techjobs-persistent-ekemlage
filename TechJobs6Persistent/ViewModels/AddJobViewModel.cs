1b4099b47a8d9aa5a5e3cb14de6ef8e1046b3eb1
Emily Kemlage <emilykemlage@gmail.com>
2023-12-04 18:52:45 -0600

    Started the "adding a job" section of task 2

---
+++ TechJobs6Persistent/ViewModels/AddJobViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TechJobs6Persistent.Models;
+
+namespace TechJobs6Persistent.ViewModels
+{
+    public class AddJobViewModel
+    {
+        public string Name { get; set;}
+        public int EmployerId { get; set;}
+
+        public List<Employer> SelectListItem { get; set;}
+    }
+}
+
+
+
